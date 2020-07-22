821b479a30cc596401f20640e0de90b0cd0fac06
Jason Williams <jase.williams@gmail.com>
2020-07-22 10:01:38 +0100

    updates

--- src/AnkiService.ts
+++ src/AnkiService.ts
@@ -1,4 +1,10 @@
 import fetch from "node-fetch";
+import * as vscode from "vscode";
+
+interface IResponse {
+  result: any;
+  error: any;
+}
 
 export class AnkiService {
   private url: string;
@@ -8,23 +14,19 @@ export class AnkiService {
     this.url = url;
   }
 
-  async invoke(action: string, params?: object) {
+  async invoke(action: string, params?: object): Promise<IResponse> {
     const req = { action, version: this.version, ...params };
-    try {
-      const response = await fetch(this.url, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req),
-      });
+    const response = await fetch(this.url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    });
 
-      const jsonResponse = await response.json();
-      console.log(jsonResponse);
-      return jsonResponse;
-    } catch (e) {
-      console.log(e);
-    }
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return jsonResponse;
   }
 
   // https://github.com/FooSoft/anki-connect/blob/master/actions/miscellaneous.md
